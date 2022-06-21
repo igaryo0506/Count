20bdf77d213dff9d2ef124d830573149225dbe3f
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2022-06-21 21:12:04 +0900

    Initial Commit

---
+++ CountApp/CountAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountAppApp.swift
+//  CountApp
+//
+//  Created by 五十嵐諒 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct CountAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
