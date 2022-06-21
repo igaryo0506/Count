69fe7b2bc93ee0116989d1b843ed49e47c0669e0
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2022-06-21 21:34:52 +0900

    better count app

--- CountApp/ContentView.swift
+++ CountApp/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number = 0
+    
     var body: some View {
         VStack{
             Text(String(number))
+                .font(.largeTitle)
             
-            Button("+"){
+            Button(action:{
                 number += 1
+            }){
+                Image(systemName: "plus")
+                    .frame(width: 60, height: 60)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
             }
-            .padding(16)
         }
     }
 }
